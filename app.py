f98887b318dd979c206cf8fa3fe0165de5f3af6e
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-07-16 18:25:36 +0100

    cf;f.;f

--- app.py
+++ app.py
@@ -2,14 +2,24 @@
 
 # import flask functionality in order to set up the application for use
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-# remember the "/" refers to the default route 
+app.config["MONGO_DBNAME"] = "task_manager"
+app.config["MONGO_URI"] = "mongodb+srv://aledgriffiths79:motoisfun38@myfirstcluster-wlh95.mongodb.net/task_manager?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-  return "Hello World ...again ITS ALED PISGAH"
+@app.route("/get_tasks")
+
+# remember the "/" refers to the default route 
+def get_tasks():
+  return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True) 
