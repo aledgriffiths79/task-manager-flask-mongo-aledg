73191e05f6e220f461de85b62cc72e01f84a0fe3
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-07-16 13:47:25 +0100

    cls

--- app.py
+++ app.py
@@ -11,13 +11,16 @@ app = Flask(__name__)
 def hello():
   return "Hello World ...again"
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
   # app.run() function we set the host, we use the os import, we use the getenv object and then we get the IP
   # The following code works for production (heroku)
-  app.run(host=os.getenv("IP"),
+  # app.run(host=os.getenv("IP"),
   # then we set the port, and we convert the port to an integer
   # The following code works for production (heroku)
-  port=int(os.getenv("PORT")))
+  # port=int(os.getenv("PORT")))
   # last parameter we want to pass is debug. By setting it to true, it allows the changes to be picked up automatically in the browser.
   # below syntax is for opening project locally not production (heroku)
   # app.run(debug=True)
+
+  if __name__ == '__main__':
+    app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True) 
