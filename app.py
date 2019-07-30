b40aba259cfa88b12f112a0ec5e309ab92ceff9f
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-07-30 15:35:55 +0100

    adding a nav bar

--- app.py
+++ app.py
@@ -78,6 +78,16 @@ def delete_category(category_id):
   mongo.db.categories.remove({'_id': ObjectId(category_id)})
   return redirect(url_for('get_categories'))
 
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+  categories = mongo.db.categories
+  category_doc = {'category_name': request.form.get('category_name')}
+  categories.insert_one(category_doc)
+  return redirect(url_for('get_categories'))
+
+@app.route('/new_category')
+def new_category():
+  return render_template('addcategory.html')
 
 if __name__ == '__main__':
     # app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True) 
